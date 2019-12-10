622b37f8c5ed47ae19a0cdc61ddbbdf4762ef644
Brent Kerby <blkerby@gmail.com>
2019-12-10 00:32:55 -0700

    continue development

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::fmt;
 use std::ops::{Index, IndexMut};
 use std::mem;
+use std::collections::{HashSet, HashMap};
 
 #[derive(Hash, Eq, PartialEq, Clone)]
 struct LowerTriangularMatrix {
     data: Vec<bool>,    // Data stored in row-major order (only the entries that can possibly be non-zero)
     dim: usize
+}
+
+impl LowerTriangularMatrix {
+    fn zeros(dim: usize) -> LowerTriangularMatrix {
+        LowerTriangularMatrix {
+            data: vec![false; dim * (dim + 1) / 2],
+            dim,
+        }
+    }
 }
 
 impl Index<[usize; 2]> for LowerTriangularMatrix {
@@ -79,14 +89,164 @@ fn generator_transvection(input: &LowerTriangularMatrix) -> LowerTriangularMatri
     return output;
 }
 
+fn canonical_matrix(dim: usize, rank: usize, kind: usize) -> LowerTriangularMatrix {
+    assert!(rank <= dim);
+    assert!(kind <= 1);
+    assert!((rank > 0 && rank % 2 == 0) || kind == 0);
+    
+    let mut output = LowerTriangularMatrix::zeros(dim);
+    let mut i = 0;    
+    if rank % 2 == 1 {
+        output[[0, 0]] = true;
+        i += 1;
+    } else if kind == 1 {
+        output[[0, 0]] = true;
+        output[[1, 0]] = true;
+        output[[1, 1]] = true;
+        i += 2;
+    }
+    while i < rank {
+        output[[i + 1, i]] = true;
+        i += 2;
+    }
+    output
+}
+
+
+struct CanonicalMatrix {
+    mat: LowerTriangularMatrix,
+    dim: usize,
+    rank: usize,
+    kind: usize,
+}
+
+fn all_canonical_matrices(dim: usize) -> Vec<CanonicalMatrix> {
+    let mut out: Vec<CanonicalMatrix> = Vec::new();
+    for rank in 0..(dim + 1) {
+        out.push(CanonicalMatrix {
+            mat: canonical_matrix(dim, rank, 0),
+            dim,
+            rank,
+            kind: 0
+        });
+        if rank % 2 == 0 && rank > 0 {
+            out.push(CanonicalMatrix {
+                mat: canonical_matrix(dim, rank, 1),
+                dim,
+                rank,
+                kind: 1
+            });
+        }
+    }   
+    out    
+}
+
+fn compute_orbit(mat: LowerTriangularMatrix) -> HashSet<LowerTriangularMatrix> {
+    let mut full_set: HashSet<LowerTriangularMatrix> = HashSet::new();
+    let mut new_set: HashSet<LowerTriangularMatrix> = HashSet::new();
+    new_set.insert(mat);
+    while !new_set.is_empty() {
+        let mut next_set: HashSet<LowerTriangularMatrix> = HashSet::new();
+        for mat in new_set {
+            let new_mat = generator_cycle(&mat);
+            if full_set.insert(new_mat.clone()) {
+                next_set.insert(new_mat);
+            }
+
+            let new_mat = generator_transvection(&mat);
+            if full_set.insert(new_mat.clone()) {
+                next_set.insert(new_mat);
+            }
+        }
+        new_set = next_set;
+    }
+    full_set
+}
+
+fn create_full_space_rec(more_dim: usize, prefix: &mut Vec<bool>, out: &mut Vec<Vec<bool>>) {
+    if more_dim == 0 {
+        out.push(prefix.clone());
+    } else {
+        prefix.push(false);
+        create_full_space_rec(more_dim - 1, prefix, out);
+        prefix.pop();
+
+        prefix.push(true);
+        create_full_space_rec(more_dim - 1, prefix, out);
+        prefix.pop();
+    }
+}
+
+fn create_full_space(dim: usize) -> Vec<Vec<bool>> {
+    let mut out: Vec<Vec<bool>> = Vec::new();
+    let mut prefix: Vec<bool> = Vec::new();
+    create_full_space_rec(dim, &mut prefix, &mut out);
+    out
+}
+
+fn evaluate(mat: &LowerTriangularMatrix, v: &[bool]) -> bool {
+    let mut out = false;
+    for i in 0..mat.dim {
+        for j in 0..i + 1 {
+            out ^= mat[[i, j]] && v[i] && v[j];
+        }
+    }
+    out
+}
+
+fn evaluate_all(mat: &LowerTriangularMatrix) -> Vec<bool> {
+    create_full_space(mat.dim).iter().map(|v| evaluate(mat, v)).collect()
+}
+
+fn compute_weight(mat: &LowerTriangularMatrix) -> usize {
+    evaluate_all(mat).iter().map(|b| *b as usize).sum()
+}
+
+fn compute_intersection_numbers(mat: &LowerTriangularMatrix, orbit: &HashSet<LowerTriangularMatrix>) -> Vec<usize> {
+    let mut counts: HashMap<usize, usize> = HashMap::new();
+    let v1 = evaluate_all(mat);
+    for m in orbit {
+        let v2 = evaluate_all(m);
+        if v2 == v1 { 
+            continue; 
+        }
+        let mut cnt = 0;
+        for (x, y) in v1.iter().zip(&v2) {
+            if *x && *y {
+                cnt += 1;
+            }
+        }
+        *counts.entry(cnt).or_insert(0) += 1;
+    }
+
+    let mut counts_vec: Vec<usize> = Vec::new();
+    for i in 0 ..= v1.len() {
+        counts_vec.push(*counts.get(&i).unwrap_or(&0));
+    }
+    counts_vec
+}
+
 fn main() {
-    let mut mat = LowerTriangularMatrix {
-        data: vec![false; 10],
-        dim: 4
-    };
-    mat.data[9] = true;
+//     let mut mat = LowerTriangularMatrix {
+//         data: vec![false; 10],
+//         dim: 4
+//     };
+//     mat.data[9] = true;
     // let mat1 = generator_cycle(&mat);
-    let mat1 = generator_transvection(&mat);
-    println!("{}", mat);
-    println!("{}", mat1);
+    // let mat1 = generator_transvection(&mat);
+    let dim = 6;
+    let mut total = 0;
+    // println!("{:?}", create_full_space(4));
+    let canonical_matrices = all_canonical_matrices(dim);
+
+    for cm in canonical_matrices {
+        let set = compute_orbit(cm.mat.clone());
+        println!("rank {}, kind {}, weight {}, orbit size {}", cm.rank, cm.kind, compute_weight(&cm.mat), set.len());
+        println!("intersections: {:?}", compute_intersection_numbers(&cm.mat, &set));
+        total += set.len();
+    }   
+    println!("total: {}", total);
+    // for m in set {
+        // println!("{}", m);
+    // }
 }
